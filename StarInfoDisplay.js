7c8d2abe747568cab1e4c21281d15ca59d07968f
Quentin <quentinlemaire73sdb@gmail.com>
2023-11-27 12:30:36 +0100

    try to ad closing of the popup

--- StarInfoDisplay.js
+++ StarInfoDisplay.js
@@ -5,21 +5,24 @@ class StarInfoDisplay {
     
   
     showPopup(starData) {
+      // Ajout d'un bouton de fermeture
+      const closeButton = document.createElement('button');
+      closeButton.innerText = 'X';
+      closeButton.id = 'closePopup';
+      closeButton.addEventListener('click', () => {
+        this.hidePopup();
+      });
+
+      // Ajout des données de l'étoile
       this.popupElement.innerHTML = `Name: ${starData.name}<br>
                                      Heat: ${starData.heat}<br>
                                      Size: ${starData.size}<br>
                                      Brightness: ${starData.brightness}<br>
                                      Constellation: ${starData.constellation}`;
-                                     
-      this.popupElement.style.display = 'block';
-      
-      const closeButton = document.createElement('button');
-      closeButton.innerText = 'X';
-      closeButton.addEventListener('click', () => {
-        this.hidePopup();
-      });
-      
+      // Ajout du bouton de fermeture
       this.popupElement.appendChild(closeButton);
+      // Affichage de la pop-up
+      this.popupElement.style.display = 'block';
     }
   
     hidePopup() {
